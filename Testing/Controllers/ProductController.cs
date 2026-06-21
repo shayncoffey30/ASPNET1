912cb72429dba7ec9086af5505e4abc03cb7ef1c
Shayn Coffey <Shayncoffey30@gmail.com>
2026-06-21 01:28:32 -0400

    MVC Delete product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -56,6 +56,12 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
 
 
     }
